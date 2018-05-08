b04332351f75f698f22e9902147755242bc9586e
filipnathanel <nathanel@o2.pl>
2018-05-09 01:08:44 +0200

    polish up options styles

--- app/components/Select/Option.js
+++ app/components/Select/Option.js
@@ -2,6 +2,9 @@ import React from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
+import * as colors from 'utils/colors';
+import { inputStyles } from 'components/Input';
+
 const Option = (props) => {
   const clickHandler = (e) => {
     const value = e.target.getAttribute('value');
@@ -9,13 +12,14 @@ const Option = (props) => {
     return props.onClick({ value, label });
   };
 
-  const { label, value, index } = props;
+  const { label, value, index, className } = props;
 
   return (<div
     role="menuitem"
     onClick={clickHandler}
     value={value}
     tabIndex={index}
+    className={className}
   >{label}</div>
   );
 };
@@ -26,6 +30,17 @@ Option.propTypes = {
   value: PropTypes.string.isRequired,
   label: PropTypes.string.isRequired,
   index: PropTypes.number,
+  className: PropTypes.string.isRequired,
 };
 
-export default styled(Option)``;
+export default styled(Option)`
+  ${inputStyles}
+  cursor:pointer;
+  margin-bottom: 0;
+  color: ${colors.darkBlue};
+
+  &:last-child {
+    border-bottom:none;
+  }
+}
+`;
--- app/components/Select/Options.js
+++ app/components/Select/Options.js
@@ -21,19 +21,22 @@ const Options = (props) => {
 Options.propTypes = {
   options: PropTypes.array,
   onClick: PropTypes.func,
+  className: PropTypes.string,
 };
 
 export default styled(Options)`
     position: absolute;
-    right:1em;
-    top: 2em;
+    left: 0;
+    top: 2.6em;
     background-color:white;
     box-shadow: 1px 1px 4px rgba(0,0,0,0.2);
     min-width:300px;
-    padding:0.5em;
+    width:100%;
+    padding: 0 0.5em;
     transition: opacity 0.18s, transform 0.18s;
     transform:translateY(3px);
-    opacity:${(props) => props.open ? '1' : '0'};;
-    pointer-events: ${({open}) => open ? 'auto' : 'none'};
+    opacity:${({ open }) => open ? '1' : '0'};;
+    pointer-events: ${({ open }) => open ? 'auto' : 'none'};
     z-index: 2;
+    box-shadow: 0px 2px 14px rgba(0,0,0,0.5);
 `;
