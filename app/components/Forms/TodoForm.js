6450c80a4e3ccf92c23c0aa5e85b85665b726c11
filipnathanel <nathanel@o2.pl>
2018-05-08 20:57:53 +0200

    Added TodoForm

--- app/components/Forms/TodoForm.js
+++ app/components/Forms/TodoForm.js
@@ -1,11 +1,34 @@
 import React from 'react';
 
+import Wrap from 'components/Wrap';
+import Form from 'components/Form';
+import Select from 'components/Select';
+import Input from 'components/Input';
+
 const TodoForm = (props) => {
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log(e.target);
+  };
+
   return (
-    <form>
-      <input type="text" placeholder="What are you going to do?" />
-      <input type="text" placeholder="where" />
-    </form>
+    <Wrap>
+      <Form onSubmit={submitHandler}>
+        <Select
+          id="type"
+          placeholder="Select Type"
+          options={[
+            { value: 'personal', label: 'Personal' },
+            { value: 'business', label: 'Business' },
+          ]}
+        />
+        <Input id="name" type="text" placeholder="What are you going to do?" />
+        <Input id="place" type="text" placeholder="Where?" />
+        <button type="submit">
+        AddTodo</button>
+
+      </Form>
+    </Wrap>
   );
 };
 
