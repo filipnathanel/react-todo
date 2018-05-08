81e31e7e68e936cf4eb9e71873dc905055af6dd8
filipnathanel <nathanel@o2.pl>
2018-05-08 20:58:06 +0200

    Limit the wrapper size

--- app/components/Wrap/index.js
+++ app/components/Wrap/index.js
@@ -2,6 +2,8 @@ import styled from 'styled-components';
 
 export default styled.div`
   display: flex;
-  padding: 0 3.5vw;
+  padding: 0 3.5%;
+  max-width: 481px;
+  margin: 0 auto;
   // background-color: #fff;
 `;
