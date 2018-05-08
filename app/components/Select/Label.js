45ebeca24c524d59a7d839f928c7858f77727957
filipnathanel <nathanel@o2.pl>
2018-05-09 01:08:58 +0200

    Add arrow to Select Label

--- app/components/Select/Label.js
+++ app/components/Select/Label.js
@@ -2,4 +2,25 @@ import styled from 'styled-components';
 
 import { inputStyles } from 'components/Input';
 
-export default styled.div`${inputStyles}`;
+export default styled.div`
+
+  ${inputStyles}
+  position:relative;
+  cursor:pointer;
+
+  &:before{
+    content: "";
+    display: block;
+    width: 0;
+    height: 0;
+    border-style: solid;
+    position: absolute;
+    top: 50%;
+    margin-top: -3px;
+    right: 3px;
+    border-width: 5px 5px 0;
+    border-color: #83ABD1 transparent transparent;
+    transform: ${({ open }) => open ? 'rotate(180deg)' : 'none' };
+  }
+
+`;
