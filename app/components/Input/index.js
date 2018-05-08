55cd1c7a07f4cb169b071fadb94f6124f9ab5e7d
filipnathanel <nathanel@o2.pl>
2018-05-09 01:09:25 +0200

    polish up Input styles

--- app/components/Input/index.js
+++ app/components/Input/index.js
@@ -6,13 +6,20 @@ export const inputStyles = `
   font-weight: 500;
   display: block;
   width: 100%;
-  padding: 0.5em 0;
-  margin-bottom: 0.2em;
-  border-bottom: 1px solid ${colors.lightBlue};
+  padding: 0.66em 0;
+  margin-bottom: 1em;
+  border-bottom: 1px solid ${colors.paleBlue};
+  color: white;
 
   &:focus{
     outline:none;
   }
+
+  &::-webkit-input-placeholder{
+    color: ${colors.paleBlue};
+    opacity:1;
+  }
+
 `;
 
 export default styled.input`${inputStyles}`;
