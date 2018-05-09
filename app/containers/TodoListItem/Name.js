ffa01b7fd2a37bb853f034e3e238bbd7b02b0fab
filipnathanel <nathanel@o2.pl>
2018-05-10 00:58:49 +0200

    Add linethrough completed TodoListItem Name

--- app/containers/TodoListItem/Name.js
+++ app/containers/TodoListItem/Name.js
@@ -3,4 +3,5 @@ import styled from 'styled-components';
 export default styled.div`
   font-weight: 500;
   margin-top:0.15em;
+  text-decoration: ${({ completed }) => completed ? 'line-through' : 'none'}
 `;
