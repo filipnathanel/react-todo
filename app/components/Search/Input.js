8240d4fb8aca374aebad6f3a0775ca8ca12cf979
filipnathanel <nathanel@o2.pl>
2018-05-10 00:56:57 +0200

    Connect Search Input to store

--- app/components/Search/Input.js
+++ app/components/Search/Input.js
@@ -1,7 +1,12 @@
 import styled from 'styled-components';
 import NormalInput from 'components/Input';
+import { compose } from 'redux';
+import { connect } from 'react-redux';
+import reducer from 'containers/TodoPage/reducer';
+import { setSearch } from 'containers/TodoPage/actions';
+import injectReducer from 'utils/injectReducer';
 
-export default styled(NormalInput)`
+const Input = styled(NormalInput)`
   display: inline-block;
   width:auto;
   vertical-align:middle;
@@ -11,3 +16,19 @@ export default styled(NormalInput)`
   color: black;
   margin-bottom:0;
 `;
+
+function mapDispatchToProps(dispatch) {
+  return {
+    onChange: (e) => {
+      dispatch(setSearch(e.target.value));
+    },
+  };
+}
+
+const withReducer = injectReducer({ key: 'todos', reducer });
+const withConnect = connect(null, mapDispatchToProps);
+
+export default compose(
+  withReducer,
+  withConnect
+)(Input);
