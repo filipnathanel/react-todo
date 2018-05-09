f96bfc6dd3fb6e7f604bb9902086d86bd0cd2d8e
filipnathanel <nathanel@o2.pl>
2018-05-09 18:46:56 +0200

    Add filter Related action Const names

--- app/containers/TodoPage/constants.js
+++ app/containers/TodoPage/constants.js
@@ -11,3 +11,8 @@ export const ADD_TODO = 'todo/TodoPage/ADD_TODO';
 export const EDIT_TODO = 'todo/TodoPage/EDIT_TODO';
 export const REMOVE_TODO = 'todo/TodoPage/REMOVE_TODO';
 export const TOGGLE_TODO = 'todo/TodoPage/TOGGLE_TODO';
+export const SET_VISIBILITY_FILTER = 'todo/TodoPage/SET_VISIBILITY_FILTER';
+
+export const SHOW_ALL = 'todo/TodoPage/filter/ALL';
+export const SHOW_COMPLETED = 'todo/TodoPage/filter/COMPLETED';
+export const SHOW_ACTIVE = 'todo/TodoPage/filter/ACTIVE';
