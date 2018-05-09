8185d39987b60fd7c6c84147edeaddc433ed9684
filipnathanel <nathanel@o2.pl>
2018-05-09 14:10:02 +0200

    activeTodo router selector

--- app/containers/TodoPage/selectors.js
+++ app/containers/TodoPage/selectors.js
@@ -4,7 +4,7 @@ import { createSelector } from 'reselect';
  * Direct selector to the todoPage state domain
  */
 const selectTodosDomain = (state) => state.get('todos');
-
+const selectActiveTodoId = (state, props) => props.match.params.id;
 /**
  * Other specific selectors
  */
@@ -24,10 +24,25 @@ const makeSelectTodos = () => createSelector(
   (todosState) => todosState.get('todos').toJS()
 );
 
+const getActiveTodo = () => createSelector(
+  makeSelectTodos(),
+  selectActiveTodoId,
+  (todos, id) => {
+    if (id) {
+      return todos.filter((todo) => todo.id === id)[0];
+    }
+    return false;
+  }
+);
+
+// makeSelectTodo = (id) => createSelector([getTodo])
+
 export default makeSelectTodos;
 export {
   selectTodosDomain,
   makeSelectLoading,
   makeSelectError,
   makeSelectTodos,
+  getActiveTodo,
 };
+
